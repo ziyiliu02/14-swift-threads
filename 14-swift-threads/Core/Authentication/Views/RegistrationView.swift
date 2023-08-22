bd2d8acf3f068815acdb0498c02182d5d6fdd263
Liu Ziyi <ziyi.liu02@gmail.com>
2023-08-22 17:12:50 +0800

    - Authentication UI

---
+++ 14-swift-threads/Core/Authentication/Views/RegistrationView.swift
@@ -0,0 +1,81 @@
+//
+//  RegistrationView.swift
+//  14-swift-threads
+//
+//  Created by Liu Ziyi on 22/8/23.
+//
+
+import SwiftUI
+
+struct RegistrationView: View {
+    
+    @State private var email = ""
+    @State private var password = ""
+    @State private var fullname = ""
+    @State private var username = ""
+    
+    var body: some View {
+        VStack {
+           Spacer()
+            
+            Image(systemName: "bubble.left.and.bubble.right")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 120, height: 120)
+                .padding()
+            
+            VStack {
+                TextField("Enter your email", text: $email)
+                    .autocapitalization(.none)
+                    .modifier(ThreadsTextFieldModifier())
+                
+                SecureField("Enter your password", text: $password)
+                    .modifier(ThreadsTextFieldModifier())
+                
+                TextField("Enter your full name", text: $fullname)
+                    .modifier(ThreadsTextFieldModifier())
+                
+                TextField("Enter your username", text: $username)
+                    .modifier(ThreadsTextFieldModifier())
+            }
+            
+            Button {
+                
+            } label: {
+                Text("Sign Up")
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white)
+                    .frame(width: 352, height: 44)
+                    .background(.black)
+                    .cornerRadius(8)
+            }
+            .padding(.vertical)
+            
+            Spacer()
+            
+            Divider()
+            
+            Button {
+                
+            } label: {
+                HStack(spacing: 3) {
+                    Text("Already have an account?")
+                    
+                    Text("Sign In")
+                        .fontWeight(.semibold)
+                }
+                .foregroundColor(.black)
+                .font(.footnote)
+            }
+            .padding(.vertical, 16)
+
+        }
+    }
+}
+
+struct RegistrationView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegistrationView()
+    }
+}
---
+++ 14-swift-threads/Utils/ViewModifiers/ThreadsTextFieldModifier.swift
@@ -0,0 +1,19 @@
+//
+//  ThreadsTextFieldModifier.swift
+//  14-swift-threads
+//
+//  Created by Liu Ziyi on 22/8/23.
+//
+
+import SwiftUI
+
+struct ThreadsTextFieldModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.subheadline)
+            .padding(12)
+            .background(Color(.systemGray6))
+            .cornerRadius(10)
+            .padding(.horizontal, 24)
+    }
+}
