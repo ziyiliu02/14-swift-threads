bebd87b8d228b846101e13ce20c8a824a9ea0dbd
Liu Ziyi <ziyi.liu02@gmail.com>
2023-08-22 22:25:25 +0800

    - Feed UI

--- 14-swift-threads/Core/Feed/Views/FeedView.swift
+++ 14-swift-threads/Core/Feed/Views/FeedView.swift
@@ -9,7 +9,31 @@ import SwiftUI
 
 struct FeedView: View {
     var body: some View {
-        Text("Feed")
+        NavigationStack {
+            ScrollView(showsIndicators: false) {
+                LazyVStack {
+                    ForEach(0 ... 10, id: \.self) { thread in
+                        ThreadCell()
+                    }
+                }
+            }
+            .refreshable {
+                print("DEBUG: Refresh threads")
+            }
+            .navigationTitle("Threads")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "arrow.counterclockwise")
+                            .foregroundColor(.black)
+                    }
+
+                }
+            }
+        }
     }
 }
 
