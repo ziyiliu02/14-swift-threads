364d119f6913e63ea8dd7975adbb06dd559f940b
Liu Ziyi <ziyi.liu02@gmail.com>
2023-08-22 13:17:45 +0800

    Initial Commit

---
+++ 14-swift-threads/_4_swift_threadsApp.swift
@@ -0,0 +1,17 @@
+//
+//  _4_swift_threadsApp.swift
+//  14-swift-threads
+//
+//  Created by Liu Ziyi on 22/8/23.
+//
+
+import SwiftUI
+
+@main
+struct _4_swift_threadsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
