ec9f3ea3421de9ccfed783f6ab44b33adc2415cf
Liu Ziyi <ziyi.liu02@gmail.com>
2023-08-22 14:43:57 +0800

    - Project Setup

---
+++ 14-swift-threads/Core/Authentication/Views/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  14-swift-threads
+//
+//  Created by Liu Ziyi on 22/8/23.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
