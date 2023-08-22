1e82f7cf2c8bb56451a3b7d606473459bbeee261
Liu Ziyi <ziyi.liu02@gmail.com>
2023-08-22 14:02:53 +0800

    - Firebase Setup

--- 14-swift-threads/_4_swift_threadsApp.swift
+++ 14-swift-threads/_4_swift_threadsApp.swift
@@ -5,10 +5,16 @@
 //  Created by Liu Ziyi on 22/8/23.
 //
 
+import FirebaseCore
 import SwiftUI
 
 @main
 struct _4_swift_threadsApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
